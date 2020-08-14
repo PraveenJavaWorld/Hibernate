5a568e957dd06eacecc6fc42b2daa8fabc6c631f
PraveenJavaWorld <praveen97javaworld@gmail.com>
2020-08-14 17:04:56 +0530

    First Commit

--- HBProj1-PersistObject/src/com/nt/test/PersistObjectTest.java
+++ HBProj1-PersistObject/src/com/nt/test/PersistObjectTest.java
@@ -8,7 +8,7 @@ import org.hibernate.cfg.Configuration;
 
 import com.nt.entity.Product;
 
-public class SaveObjectTest {
+public class PersistObjectTest {
 
 	public static void main(String[] args) {
 		
@@ -18,25 +18,31 @@ public class SaveObjectTest {
 		Transaction tx = null;
 		Product prod = null;
 		boolean flag = false;
+		long idVal = 0;
 		//Activate Hibernate f/w (BootStrap hibernate)
 		cfg = new Configuration();
+		System.out.println(cfg.getProperties());
+	    System.out.println("...........................");
 		//supply hiberante cfg file as input file
 		cfg.configure("com/nt/cfgs/hibernate.cfg.xml");
+		System.out.println(cfg.getProperties());
 		//build Session Factory
 		factory = cfg.buildSessionFactory();
+		System.out.println("SessionFactory object class name::"+factory.getClass());
 		//open Session
 		ses = factory.openSession();
+		System.out.println("Session object class name::"+ses.getClass());
 		//create Entity  object to save with Db s/w
 		prod = new Product();
-		prod.setPid(123);
-		prod.setPname("table");
-		prod.setPrice(60000);
+		prod.setPid(789);
+		prod.setPname("CPU");
+		prod.setPrice(75000);
 		prod.setQty(2);
 		
 		try {
 			tx = ses.beginTransaction();//internally calls  con.setAutoCommit(false) to begin the Tx
 			//save object
-			ses.save(prod);
+			ses.persist(prod);
 			flag = true;
 			
 		} catch (HibernateException he) {
@@ -50,7 +56,7 @@ public class SaveObjectTest {
 				System.out.println("Object is Saved");
 			}
 			else {
-				tx.rollback();//internally calls  con.rollback()
+				tx.rollback(); //internally calls  con.rollback()
 				System.out.println("Object is not Saved");
 			}
 			//close  session object
