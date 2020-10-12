36e5258edc60e37d6b02db2a6e532b9378476bea
PraveenJavaWorld <praveen97javaworld@gmail.com>
2020-10-12 17:29:09 +0530

    Connection Pools

---
+++ HBProj29-Standalone-JDBC-ConPools-Agroal/src/main/java/com/nt/entity/InsurancePolicy.java
@@ -0,0 +1,57 @@
+package com.nt.entity;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class InsurancePolicy implements Serializable {
+	
+	//bean properties
+	public Long policyId;
+	public String policyName;
+	public String policyType;
+	public String company;
+	public Integer tenure;
+	
+	//getters & setters
+	public Long getPolicyId() {
+		return policyId;
+	}
+	public void setPolicyId(Long policyId) {
+		this.policyId = policyId;
+	}
+	public String getPolicyName() {
+		return policyName;
+	}
+	public void setPolicyName(String policyName) {
+		this.policyName = policyName;
+	}
+	public String getPolicyType() {
+		return policyType;
+	}
+	public void setPolicyType(String policyType) {
+		this.policyType = policyType;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public Integer getTenure() {
+		return tenure;
+	}
+	public void setTenure(Integer tenure) {
+		this.tenure = tenure;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "InsurancePolicy Details : \npolicyId=" + policyId + ", \npolicyName=" + policyName + ", \npolicyType=" + policyType
+				+ ", \ncompany=" + company + ", \ntenure=" + tenure;
+	}
+	
+	
+	
+
+}
