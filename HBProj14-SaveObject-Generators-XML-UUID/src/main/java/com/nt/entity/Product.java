935fef6928628aa00ef84f1eb08c9d6c01d8104e
PraveenJavaWorld <praveen97javaworld@gmail.com>
2020-12-23 17:35:03 +0530

    uuid generator

---
+++ HBProj14-SaveObject-Generators-XML-UUID/src/main/java/com/nt/entity/Product.java
@@ -0,0 +1,15 @@
+package com.nt.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+	// bean properties
+	private String pid;
+	private String pname;
+	private Float price;
+	private Float qty;
+
+	
+}
