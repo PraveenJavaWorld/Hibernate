e4a19c070839632bcdf43f0df8f4a0ce44760345
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-04-28 20:10:59 +0530

    Object Versioning XML

---
+++ HBProj24-ObjectVersioning-XML/src/main/java/com/nt/test/VersionInsertionTest.java
@@ -0,0 +1,57 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.nt.entity.MobileCustomer;
+import com.nt.utility.HibernateUtil;
+
+public class VersionInsertionTest {
+
+	public static void main(String[] args) {
+		
+		Session session = null;
+		MobileCustomer cust = null;
+		Transaction tx = null;
+		boolean flag = false;
+		Integer idVal = null;
+		//get session
+		session = HibernateUtil.getSession();
+		//prepare object
+		cust = new MobileCustomer();
+		cust.setCname("Praveen");
+		cust.setMobileNumber(1234567890L);
+		cust.setCallerTune("Baahubali Title Song");
+		try {
+			//begin tx
+			tx = session.beginTransaction();
+			idVal = (Integer)session.save(cust);
+			System.out.println("Generated ID Value :: "+idVal);
+			flag = true;
+		} 
+		catch (HibernateException he) {
+			he.printStackTrace();
+			flag = false;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			flag = false;
+		}
+		finally {
+			if(flag) {
+				tx.commit();
+				System.out.println("Record is Saved");
+			}
+			else {
+				tx.rollback();
+				System.out.println("Record is Not Saved");
+			}
+			//close objs
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}//finally
+
+	}//main
+
+}//class
