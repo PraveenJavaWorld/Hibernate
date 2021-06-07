d552663182c9a596ffdf0ef3b36cfcf867e47a3a
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-06-07 22:21:33 +0530

    Bootstrap HB Modern CustomService

---
+++ HBProj37-Bootstrapping-ModernApproach-CustomService/src/main/java/com/nt/test/LoadObjectTest.java
@@ -0,0 +1,41 @@
+package com.nt.test;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import com.nt.entity.InsurancePolicy;
+import com.nt.utility.HibernateUtil;
+
+public class LoadObjectTest {
+
+	public static void main(String[] args) {
+		
+		Session session = null;
+		InsurancePolicy policy = null;
+		//get Session object
+		session = HibernateUtil.getSession();
+		
+		try {
+			//load object
+			policy = (InsurancePolicy) session.get(InsurancePolicy.class,1001L);
+			//verify
+			if(policy==null)
+				System.out.println("Record Not Found");
+			else {
+				System.out.println("Record is Found and Displayed Below");
+				System.out.println(policy);
+			}
+		} catch (HibernateException he) {
+			he.printStackTrace();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+
+	}
+
+}
