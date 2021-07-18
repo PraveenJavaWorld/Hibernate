46aa28653ed8df0a818b445361e2936d143adfae
PraveenJavaWorld <praveen97javaworld@gmail.com>
2021-07-18 18:49:51 +0530

    HB Inheritance Mapping TPSC Annotation

---
+++ HBProj61Anno-InheritanceMapping-TPSC/src/main/java/com/nt/entity/ChequePayment.java
@@ -0,0 +1,30 @@
+package com.nt.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(callSuper = true)
+@Entity
+@Table(name = "INH_CHEQUEPAYMENT_TPSC")
+@PrimaryKeyJoinColumn(name = "PAYMENT_ID",referencedColumnName = "PID")
+public class ChequePayment extends Payment {
+	
+	@Type(type = "long")
+	private Long chequeNumber;
+	@Column(length = 10)
+	@Type(type = "string")
+	private String chequeType;
+	private LocalDate expiryDate;
+
+}
